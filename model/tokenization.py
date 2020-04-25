807af14505dc2ea88135429d51d67434c878ab91
Eray Yıldız <yildizeray@hotmail.com.tr>
2020-04-25 18:07:01 +0300

    Turkish lower case

--- model/tokenization.py
+++ model/tokenization.py
@@ -146,7 +146,7 @@ class BasicTokenizer(object):
 
   def tokenize(self, text):
     """Tokenizes a piece of text."""
-    text = convert_to_unicode(text)
+    # text = convert_to_unicode(text)
     # text = self._clean_text(text)
 
     # This was added on November 1st, 2018 for the multilingual and Chinese
